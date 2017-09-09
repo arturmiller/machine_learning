fb096d4fae35338ae607b969012bca97c462f398
unknown <amiller89@gmx.de>
2017-09-09 15:26:56 +0200

    Added PytestPlugin notebook

---
+++ templates/TestSpecialAdd.py
@@ -0,0 +1,15 @@
+
+def test_a():
+    assert special_add(2, 3, 4) == 9  # @UndefinedVariable
+
+
+def test_b():
+    assert special_add(1, 1, 0) == 2  # @UndefinedVariable
+
+
+def test_c():
+    assert special_add(10, 2, 4) == 16  # @UndefinedVariable
+
+
+def test_d():
+    assert special_add(0, -5, 6) == 1  # @UndefinedVariable
